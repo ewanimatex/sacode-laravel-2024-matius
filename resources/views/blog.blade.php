8d48e576eb79b52482b4cdfc7c4683e743e577d0
Matius ewani <ewanimatex@gmail.com>
2024-10-21 04:02:57 -0700

    Belajar Rut

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+    <h1>Halaman Blog</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non atque, commodi id, praesentium voluptatum optio aperiam sed consequatur illum repudiandae voluptatem deserunt expedita dicta dignissimos ipsum temporibus, ex laudantium beatae quod cumque? Distinctio possimus optio doloribus, error rerum ducimus mollitia pariatur illum aliquam asperiores odio explicabo. Perferendis molestias unde quo.</p>
+    
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Halaman Contact</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vero quis eum inventore, corrupti necessitatibus illo ipsam minima distinctio quos error veniam, corporis officia at blanditiis? Quod, ducimus. Dicta amet corrupti nulla eaque sint veniam ea, debitis iusto voluptatum molestiae? At natus laborum eius cumque nemo eum repudiandae doloribus totam?</p>
+</body>
+</html>
